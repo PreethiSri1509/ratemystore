4d1544fd2ce7166d711e95add45bb7a8f617f88c
PreethiSri1509 <preethisri2k4@gmail.com>
2025-09-19 00:24:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage"; 
 import AdminDashboard from "./pages/AdminDashboard";
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} /> 
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
